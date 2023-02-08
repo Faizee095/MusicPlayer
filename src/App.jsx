4896d0f2904ef26cf7d961c3e65672bfceddf3cb
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-08 13:51:34 +0530

    Base Setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Discover} from './pages';
+import { Route, Routes } from 'react-router-dom';
+
+import { Searchbar, Sidebar } from './components';
+
+const App = () => {
+
+  return (
+    <div className="relative flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <Searchbar />
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
